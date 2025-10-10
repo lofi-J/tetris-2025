784f15bf0304b3c5c21c86984bd63817c4c28767
lofi <jsj2505@gmail.com>
2025-10-10 15:20:03 +0900

    [feat] grid board 10*20

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -4,16 +4,11 @@ import { JavascriptCell } from "./cell";
 export const JavascriptBoard = ({ board }: { board: Board }) => {
   return (
     <div className="grid grid-cols-10">
-      {board.map((row, colIndex) => (
-        <div key={`js-board-${row[colIndex]}-${colIndex}`}>
-          {row.map((cell, cellIndex) => (
-            <JavascriptCell
-              key={`js-board-${row[colIndex]}-${cellIndex}`}
-              cell={cell}
-            />
-          ))}
-        </div>
-      ))}
+      {board.map((row, rowIndex) =>
+        row.map((col, colIndex) => (
+          <JavascriptCell key={`${rowIndex}-${colIndex}`} cell={col} />
+        )),
+      )}
     </div>
   );
 };
--- src/components/cell.tsx
+++ src/components/cell.tsx
@@ -1,5 +1,5 @@
 import type { Cell } from "../hooks/use-board";
 
 export const JavascriptCell = ({ cell }: { cell: Cell }) => {
-  return <div className="cell-size border border-red-500"></div>;
+  return <div className="cell-size border border-red-500 text-[10px]"></div>;
 };
